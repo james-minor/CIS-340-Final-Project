5f019d81a82148e5f51b07e978a33fb038fbedf9
james-minor <james.minor.business@gmail.com>
2022-05-05 17:03:59 -0500

    Added Connection static class.

---
+++ Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    public static class Connection
+    {
+        public static string ConnectionString =>
+            @"Data Source=(LocalDB)\MSSQLLocalDB;
+             AttachDbFilename='|DataDirectory|\ShopEasyDatabase.mdf';Integrated Security=True";
+    }
+}
