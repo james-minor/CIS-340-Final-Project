8298cf198b05371b3f8790905caeb86056b1a059
james-minor <james.minor.business@gmail.com>
2022-05-06 09:19:28 -0500

    Gave the Create Product and Delete Product buttons proper internal names.

--- AdminPortal.Designer.cs
+++ AdminPortal.Designer.cs
@@ -32,8 +32,8 @@
             this.ProductPage = new System.Windows.Forms.TabPage();
             this.ProductOutputLabel = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.ProductDeleteButton = new System.Windows.Forms.Button();
+            this.ProductCreateButton = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.ProductUpdateCategoryInput = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -98,8 +98,8 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.ProductDeleteButton);
+            this.groupBox1.Controls.Add(this.ProductCreateButton);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.ProductUpdateCategoryInput);
             this.groupBox1.Controls.Add(this.label5);
@@ -115,23 +115,25 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Create Product";
             // 
-            // button3
+            // ProductDeleteButton
             // 
-            this.button3.Location = new System.Drawing.Point(382, 65);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(258, 23);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "Delete Product";
-            this.button3.UseVisualStyleBackColor = true;
+            this.ProductDeleteButton.Location = new System.Drawing.Point(382, 65);
+            this.ProductDeleteButton.Name = "ProductDeleteButton";
+            this.ProductDeleteButton.Size = new System.Drawing.Size(258, 23);
+            this.ProductDeleteButton.TabIndex = 9;
+            this.ProductDeleteButton.Text = "Delete Product";
+            this.ProductDeleteButton.UseVisualStyleBackColor = true;
+            this.ProductDeleteButton.Click += new System.EventHandler(this.ProductDeleteButton_Click);
             // 
-            // button2
+            // ProductCreateButton
             // 
-            this.button2.Location = new System.Drawing.Point(118, 65);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(258, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Create / Update Product";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ProductCreateButton.Location = new System.Drawing.Point(118, 65);
+            this.ProductCreateButton.Name = "ProductCreateButton";
+            this.ProductCreateButton.Size = new System.Drawing.Size(258, 23);
+            this.ProductCreateButton.TabIndex = 8;
+            this.ProductCreateButton.Text = "Create / Update Product";
+            this.ProductCreateButton.UseVisualStyleBackColor = true;
+            this.ProductCreateButton.Click += new System.EventHandler(this.ProductCreateButton_Click);
             // 
             // label6
             // 
@@ -333,8 +335,8 @@
         private System.Windows.Forms.Button ProductSearchButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox ProductSearchNameInput;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ProductDeleteButton;
+        private System.Windows.Forms.Button ProductCreateButton;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox ProductUpdateCategoryInput;
         private System.Windows.Forms.Label label5;
